f2e7e1c82898199c74b28d820ecc9ff67a433c7b
lucadambra990 <lucadambra422@gmail.com>
2025-02-18 10:17:52 +0100

    Soluzione esercizio con matchStatement

--- Lezione_03/matchStatement.py
+++ Lezione_03/matchStatement.py
@@ -1,10 +1,23 @@
 #Dato un numero restituire il suo valore cardinale
-position:int = int(input("Inserisci la posizione in cui ti sei posizionato: "))
-if position == 1:
-    print(f"{position}st!")
-elif position == 2:
-    print(f"{position}nd!")
-elif position == 3:
-    print(f"{position}rd!")
-else:
-    print(f"{position}th!")
+# position:int = int(input("Inserisci la posizione in cui ti sei posizionato: "))
+# if position == 1:
+#     print(f"{position}st!")
+# elif position == 2:
+#     print(f"{position}nd!")
+# elif position == 3:
+#     print(f"{position}rd!")
+# else:
+#     print(f"{position}th!")
+
+#Soluzione col MatchStatement
+pos:int = int(input("Inserisci la posizione in cui ti sei posizionato: "))
+
+match pos:
+    case 1:
+        print(f"{pos}st!")
+    case 2:
+        print(f"{pos}nd!")
+    case 3:
+        print(f"{pos}rd!")
+    case _:
+        print(f"{pos}th!")
