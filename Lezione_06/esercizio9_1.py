f8a335f692f8f02d3b7e90584c6b13ad8f6f0537
lucadambra990 <lucadambra422@gmail.com>
2025-03-24 19:29:25 +0100

    esercizi lezione 6

---
+++ Lezione_06/esercizio9_1.py
@@ -0,0 +1,7 @@
+class Restaurant:
+    
+    def __init__(self, name:str, cuisine_type:str):
+        self.name = name
+        self.cuisine_type = cuisine_type
+
+ristorante:Restaurant = Restaurant("Al vecchio capanaccio","mediterraneo")
