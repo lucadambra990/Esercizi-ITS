f1d8a33ac27bb1d8014bbd238069cd22415f3018
lucadambra990 <lucadambra422@gmail.com>
2025-04-15 13:58:26 +0200

    esempio docker

---
+++ Lezione_23/app.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def main():
+    matrix = np.array([
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ])
+
+    print("Matrice di partenza: ")
+    print(matrix)
+
+    print("\n\nMatrice trasportata: ")
+    transposed_matrix = matrix.T
+    print(transposed_matrix)
+
+
+
+
+if __name__ == '__main__':
+    main()
