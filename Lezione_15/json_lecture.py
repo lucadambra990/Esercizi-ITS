f1bab3841a8e18e3c5caeda2ee212dbfa58feea8
lucadambra990 <lucadambra422@gmail.com>
2025-05-09 18:46:56 +0200

    Recap file + spiegazione json

---
+++ Lezione_15/json_lecture.py
@@ -0,0 +1,20 @@
+import json
+
+PATH:str = "db.json"
+mode:str = "r"
+encoding:str="utf-8"
+
+
+config=None
+
+with open(PATH,mode=mode,encoding=encoding) as file:
+
+   config:dict = json.load(file)
+
+
+config["AGGIUNTA"] = "NUOVO"
+config["dati_persona"]["nome"] = "Gigio"
+
+with open(PATH, mode="w") as file:
+   json.dump(config, file, indent=4) #indent = numero di spazi
+   
