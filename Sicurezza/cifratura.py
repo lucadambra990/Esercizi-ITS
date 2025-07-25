f6b5c24669370901131fb05104703ef3f0952ec9
lucadambra990 <lucadambra422@gmail.com>
2025-07-25 13:21:30 +0200

    test algoritmo cifratura

--- Sicurezza/cifratura.py
+++ Sicurezza/cifratura.py
@@ -4,4 +4,4 @@ def cifratura(s:str) -> int:
         print(f"il valore originale della stringa: {i}")
         print(f"Il valore con lo xor 57 applicato: {c ^ 57}")
         print(f"Il carattere originale è {chr(c^57 ^ 57)}") # la funzione chr, dato valore intero, restituisce il carattere associato a quel valore
-cifratura("Nel mezzo del cammin di nostra vita")
+cifratura("Forza Napoli")
