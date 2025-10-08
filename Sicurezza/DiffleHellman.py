e026c8caf318bbd9ddd01f0a31f429d67affe3f0
lucadambra990 <lucadambra422@gmail.com>
2025-10-08 16:53:21 +0200

    algoritmo DiffleHellman

---
+++ Sicurezza/DiffleHellman.py
@@ -0,0 +1,18 @@
+# import math
+# import random
+
+
+# n = random.randint(1,50)
+
+n=147213
+p=13757
+b=21
+print(pow(p,b,n))
+print(pow(211,b,n))
+
+# procedimento spiegato:
+'''
+J e K vogliono creare un chiave scambiandosi dati pubblicamente, allora i due decidono due valori N e P che si scambiamo pubblicamente e scelgono un numero segreto
+(J sceglie a, K sceglie b)
+
+'''
